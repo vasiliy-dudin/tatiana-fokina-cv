522eec2978c19f9efec14f0af8eee61d1d191398
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-12-15 04:23:16 +0400

    Adds comma

--- scripts/generate_pdf.py
+++ scripts/generate_pdf.py
@@ -2,7 +2,7 @@ from weasyprint import HTML
 
 HTML("../docs/index.html").write_pdf(
     "../docs/cv.pdf",
-    pdf_version="1.7"
+    pdf_version="1.7",
     uncompressed=True,
     pdf_variant="pdf/ua-1",
     full_fonts=True
