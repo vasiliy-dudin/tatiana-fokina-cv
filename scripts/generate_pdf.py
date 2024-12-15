524808870635139e2aa7002864b716b4e6717811
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-12-15 04:12:23 +0400

    Adds weasyprint (py) script

---
+++ scripts/generate_pdf.py
@@ -0,0 +1,11 @@
+from weasyprint import HTML
+
+HTML("../docs/index.html").write_pdf(
+    "../docs/cv.pdf",
+    pdf_version="1.7"
+    uncompressed=True,
+    pdf_variant="pdf/ua-1",
+    full_fonts=True
+)
+
+print("PDF successfully created: resume.pdf")
